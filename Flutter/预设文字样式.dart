4383af3bd0de24fa7ead1c18e44ec5baa2f64685
0wew0 <wang124yi@163.com>
2018-12-26 01:14:31 +0800

    sync

---
+++ Flutter/预设文字样式.dart
@@ -0,0 +1,14 @@
+
+final TextTheme textTheme = Theme.of(context).textTheme;
+final List<Widget> styleItems = <Widget>[
+      TextStyleItem(name: 'Display 3', style: textTheme.display3, text: 'Regular 56sp'),
+      TextStyleItem(name: 'Display 2', style: textTheme.display2, text: 'Regular 45sp'),
+      TextStyleItem(name: 'Display 1', style: textTheme.display1, text: 'Regular 34sp'),
+      TextStyleItem(name: 'Headline', style: textTheme.headline, text: 'Regular 24sp'),
+      TextStyleItem(name: 'Title', style: textTheme.title, text: 'Medium 20sp'),
+      TextStyleItem(name: 'Subheading', style: textTheme.subhead, text: 'Regular 16sp'),
+      TextStyleItem(name: 'Body 2', style: textTheme.body2, text: 'Medium 14sp'),
+      TextStyleItem(name: 'Body 1', style: textTheme.body1, text: 'Regular 14sp'),
+      TextStyleItem(name: 'Caption', style: textTheme.caption, text: 'Regular 12sp'),
+      TextStyleItem(name: 'Button', style: textTheme.button, text: 'MEDIUM (ALL CAPS) 14sp'),
+    ];
