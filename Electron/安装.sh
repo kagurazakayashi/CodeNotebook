1ff05e6bf256f814e07491ff53a76993c7d91671
神楽坂雅詩 <cyscxchope@gmail.com>
2019-02-17 15:31:47 +0800

    sync

---
+++ Electron/安装.sh
@@ -0,0 +1,4 @@
+npm i electron -g
+npm i electron-forge -g
+npm i electron-packager -g
+# C:\Users\用户名\AppData\Roaming\npm\node_modules
---
+++ WordPress/在其他源码中向head写入内容.php
@@ -0,0 +1,4 @@
+<?php add_action("wp_head","nyarukoFHead"); ?>
+
+header.php:
+function nyarukoFHead()
