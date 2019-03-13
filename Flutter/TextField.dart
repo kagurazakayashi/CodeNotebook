ff5c77793ffa07bb1ce1d1ca18f5ed97574a4176
0wew0 <wang124yi@163.com>
2019-03-13 18:34:53 +0800

    TextField
    Flutter TextField

---
+++ Flutter/TextField.dart
@@ -0,0 +1,26 @@
+参考：https://www.jianshu.com/p/5d44d26d3556
+
+TextField(
+  keyboardType: TextInputType.text,
+  decoration: InputDecoration(
+    contentPadding: EdgeInsets.all(10.0),
+    hintText: 'Search',
+    // border: OutlineInputBorder(
+    //   borderRadius: BorderRadius.circular(20),
+    //   borderSide: BorderSide(color: Colors.grey),
+    // ),
+    border: InputBorder.none,
+  ),
+  onChanged: _textFieldChanged,
+  onSubmitted: _textFieldSubmitted,
+  autofocus: false,
+),
+
+
+void _textFieldChanged(String str) {
+  print(str);
+}
+
+void _textFieldSubmitted(String str) {
+  print(str);
+}
