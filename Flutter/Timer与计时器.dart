6af315c950e3c066a21e1a38d66cb362cd6419a4
0wew0 <wang124yi@163.com>
2021-03-24 12:55:37 +0800

    Update Timer与计时器.dart

--- Flutter/Timer与计时器.dart
+++ Flutter/Timer与计时器.dart
@@ -9,3 +9,18 @@ _timer = new Timer.periodic(const Duration(seconds: 1), (timer) {
 
 //结束
 _timer.cancel();
+
+//计时器
+  void startCountdownTimer() {
+    const fiveSec = const Duration(seconds: 5);
+    var callback = (timer) => {
+          setState(() {
+            if (_subtime < 1) {
+              _timer.cancel();
+            } else {
+              _subtime = _subtime - 5;
+            }
+          })
+        };
+    _timer = Timer.periodic(fiveSec, callback);
+  }
