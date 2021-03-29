cce78a8cbfaf7aa2489bce676518c8b8710dcc40
0wew0 <wang124yi@163.com>
2021-03-29 16:06:27 +0800

    添加只删除几次的方法

--- Git/彻底删除某文件包括历史版本中.sh
+++ Git/彻底删除某文件包括历史版本中.sh
@@ -1,4 +1,5 @@
 git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch appstoremedia/rec/APPpreview65.mp4' --prune-empty --tag-name-filter cat -- --all
+# git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch appstoremedia/rec/APPpreview65.mp4' HEAD~8..HEAD //删除最近8次的提交中带有的appstoremedia/rec/APPpreview65.mp4
 git push origin master --force
 rm -rf .git/refs/original/
 git reflog expire --expire=now --all
