193510978b53ae6fd4edbb8bda611144177113ee
神楽坂雅詩 <cyscxchope@gmail.com>
2022-11-20 15:11:22 +0800

    sync

--- Batch_Windows/创建常用模式启动项.bat
+++ Batch_Windows/创建常用模式启动项.bat
@@ -1,8 +1,8 @@
-bcdedit /copy {current} /d "Windows 10 Safe Mode"
-bcdedit /copy {current} /d "Windows 10 Safe Mode with Networking"
+bcdedit /copy {current} /d "Windows 11 Safe Mode"
+bcdedit /copy {current} /d "Windows 11 Safe Mode with Networking"
 msconfig
 REM 配置上述。禁用 Hyper-V ：
-bcdedit /copy {current} /d "Windows 10 no Hyper-V"
+bcdedit /copy {current} /d "Windows 11 no Hyper-V"
 bcdedit /set {498bb229-799c-11eb-9f8b-acd53edf9321} hypervisorlaunchtype off
 
 REM 旧版 Windows:
--- Batch_Windows/增加特殊模式启动项.bat
+++
@@ -1,6 +0,0 @@
-bcdedit /copy {current} /d "Windows11 No Hyper-V"
-bcdedit /set {XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX} hypervisorlaunchtype OFF
-bcdedit /copy {current} /d "Windows11 SafeMode"
-bcdedit /copy {current} /d "Windows11 SafeMode Network"
-msconfig
-bcdedit /delete {XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX}
