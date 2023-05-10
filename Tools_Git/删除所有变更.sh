4ba427b37bc5fdc57ab2071d7f681af8262daf7e
神楽坂雅詩 <cyscxchope@gmail.com>
2023-05-10 20:17:19 +0800

    sync

--- Tools_Git/删除所有变更.sh
+++ Tools_Git/删除所有变更.sh
@@ -1,3 +1,26 @@
+# 删除本地所有未提交的更改
+git checkout . && git clean -xdf
+
+# 一般 git clean都是配合git reset 使用的
+
+# 如果你有的修改已经加入了暂存区
+git reset --hard
+git clean -xdf
+
+# git回滚到某一个版本
+git reset --hard a136c6923d882ffc9065439f33412936902a1f5d
+
+# 强制提交
+git push -f origin master
+
+# 未push但commit回退
+git reset --soft HEAD^  # 会保留代码
+git reset --hard HEAD^  # 不会保留未提交的代码，回退到上个版本
+
+# https://blog.csdn.net/lxiao_3028/article/details/123826052
+
+
+
 # git删除未跟踪文件
 # 删除 untracked files
 git clean -f
