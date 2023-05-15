e73529dc42aae5a1cbcf6e5c5ea86cd031fcd2d7
神楽坂雅詩 <cyscxchope@gmail.com>
2023-05-15 17:46:27 +0800

    sync

--- Build/编译exfat.sh
+++ Build/编译exfat.sh
@@ -1,4 +1,7 @@
-yum install fuse-devel -y
+# 免编译
+# apt install fuse-exfat exfat-utils -y
+
+apt install fuse-devel autoconf automake libtool pkg-config fuse -y
 # https://github.com/relan/exfat
 git clone https://github.com/relan/exfat.git
 cd exfat
@@ -13,3 +16,32 @@ mkdir /mnt/exfat
 mount.exfat-fuse /dev/sdb1 /mnt/exfat
 # 取消挂载
 umount /mnt/exfat
+
+# configure: line 4101: syntax error near unexpected token `UBLIO,' .
+wget -P m4 https://raw.githubusercontent.com/irungentoo/toxcore/master/m4/pkg.m4
+echo "AC_CONFIG_MACRO_DIR([m4])" >> configure.ac
+aclocal -I m4
+autoreconf -i -f
+./configure
+make
+sudo make install
+
+# No package 'fuse' found:
+git clone https://github.com/libfuse/libfuse.git
+cd libfuse
+apt install meson ninja -y
+mkdir build
+cd build
+meson setup ..
+ninja
+cd lib
+cp libfuse3.so.3.14.1 /usr/lib/libfuse3.so
+export FUSE_CFLAGS=-lfuse3
+export FUSE_LIBS=/usr/lib/libfuse3.so
+cd ..
+cd ..
+cd ..
+autoreconf --install
+./configure
+make
+sudo make install
---
+++ Tools_Lib/OpenSSL/自签泛域名证书.sh
@@ -0,0 +1,76 @@
+# 自签泛域名证书
+# 安装OpenSSL
+apt install openssl openssl-devel -y
+vim /etc/pki/tls/openssl.cnf
+# 将如下配置的注释放开,低版本的OpenSSL不支持，可以不放开注释
+# [ req ]
+# req_extensions = v3_req # The extensions to add to a certificate request
+# [ v3_req ]
+
+# Extensions to add to a certificate request
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+# 添加如下行
+subjectAltname = @SubjectAlternativename
+# 同时增加如下信息
+[SubjectAlternativename]
+DNS.1 = uuu.moe
+DNS.2 = *.uuu.moe
+
+# 开始创建CA证书
+mkdir ~/ssl
+cd ~/ssl
+# 创建CA私钥
+openssl genrsa -out CA.key 8192
+# 制作CA公钥
+# openssl req -sha256 -new -x509 -days 36500 -key CA.key -out CA.crt -config /etc/pki/tls/openssl.cnf
+# Country Name (2 letter code) [XX]:CN
+# State or Province Name (full name) []:BJ
+# Locality Name (eg, city) [Default City]:Beijing
+# Organization Name (eg, company) [Default Company Ltd]:Kagurazaka      
+# Organizational Unit Name (eg, section) []:Yashi
+# Common Name (eg, your name or your server's hostname) []:KagurazakaYashiSign CA
+# Email Address []:cxchope@163.com
+openssl req -sha256 -new -x509 -days 36500 -key CA.key -out CA.crt -config /etc/pki/tls/openssl.cnf -subj "/C=CN/ST=BJ/L=Beijing/O=Kagurazaka/OU=Yashi/CN=KagurazakaYashiSign CA/emailAddress=cxchope@163.com"
+
+# 自签发泛域名证书
+# 生成密钥
+openssl genrsa -out uuu.moe.key 8192
+# 生成证书签发请求
+openssl req -new -sha256 -key uuu.moe.key -out uuu.moe.csr -config /etc/pki/tls/openssl.cnf -subj "/C=CN/ST=BJ/L=Beijing/O=Kagurazaka/OU=Yashi/CN=*.uuu.moe/emailAddress=cxchope@163.com"
+# 查看签名请求文件信息
+openssl req -in uuu.moe.csr -text
+# 使用自签署的CA，签署crt
+openssl ca -in uuu.moe.csr -md sha256 -days 36500 -out uuu.moe.crt -cert CA.crt -keyfile CA.key -config /etc/pki/tls/openssl.cnf
+# 遇到报错: /etc/pki/CA/index.txt: No such file or directory :
+touch /etc/pki/CA/index.txt
+# 遇到报错: /etc/pki/CA/serial: No such file or directory
+echo "01" > /etc/pki/CA/serial
+
+openssl ca -in uuu.moe.csr -md sha256 -days 36500 -out uuu.moe.crt -cert CA.crt -keyfile CA.key -config /etc/pki/tls/openssl.cnf
+# Sign the certificate? [y/n]:y
+# 1 out of 1 certificate requests certified, commit? [y/n]y
+
+# 检查部署是否正常
+cat /etc/pki/CA/index.txt
+cat /etc/pki/CA/serial
+# 注：同一个域名不能签署多次；由于签署了*.uuu.moe，且已经被记录，因此不能再次被签署。除非删除该记录。
+# 注：注意index.txt文件和serial文件的关系。serial文件内容为index.txt文件内容行数加1。
+
+# 查看证书信息
+openssl x509 -in uuu.moe.crt -text
+# 验证签发证书是否有效
+openssl verify -CAfile CA.crt uuu.moe.crt
+
+# 转换证书给Nginx使用
+openssl x509 -in uuu.moe.crt -out uuu.moe.pem -outform PEM
+
+# ls
+# CA.crt
+# CA.key
+# uuu.moe.crt
+# uuu.moe.csr
+# uuu.moe.key
+# uuu.moe.pem
+
+# https://www.cnblogs.com/layzer/articles/openssl.html
