6f3a143097a5ece62bfda87dde56b203e59e7516
KagurazakaMiyabi (神楽坂雅詩) <miyabi@uuu.moe>
2024-01-27 20:11:38 +0800

    sync

--- Network/局域网测速.sh
+++ Network/局域网测速.sh
@@ -1,7 +1,26 @@
 # 局域网测速
 
-# 服务器
+## 服务器
 iperf3 -s
+# -s:服务器  默认 TCP:5201
 
-# 客户端
-iperf3 -c IP
+## 客户端
+iperf3 -c 192.168.1.2 -b 100M -R
+# -c:客户端  -b:以100Mbps为数据发送速率  -d:双向测试
+# 不加 -R 测试上传，加 -R 测试下载
+
+## Docker 启动， -s 及其开始是 iperf3 程序的参数
+sudo docker run -d --name=iperf3-server -p 5201:5201 networkstatic/iperf3 -s
+## Docker 停止
+sudo docker stop iperf3-server && sudo docker rm iperf3-server
+
+# 带宽性能测试
+
+## 服务器
+iperf -s -u
+# -u:UDP  -s:服务器  默认 UDP:5001
+
+## 客户端
+iperf -c 192.168.1.2 -u -b 100M -t 10
+# -c:客户端  -u:UDP -b:以100Mbps为数据发送速率 -t:10秒
+# 看丢包率和实际带宽
--- Server/Docker/Portainer更新.sh
+++ Server/Docker/Portainer更新.sh
@@ -13,6 +13,12 @@ docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /va
 # client version 1.41 is too new 则用旧版本
 docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.16.2
 
+# NAS
+sudo docker stop PortainerCE && sudo docker rm PortainerCE
+sudo docker rmi portainer/portainer-ce
+sudo docker run -d --name PortainerCE -p 19800:8000 -p 19900:9000 -p 19943:9443 --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock -v /usr/builtin/etc/certificate:/certs -v /share/Docker/PortainerCE/data:/data -v /etc/localtime:/etc/localtime -e PATH:"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+" portainer/portainer-ce --sslcert /certs/ssl.pem --sslkey /certs/ssl.pem
+
 # 离线更新
 docker stop portainer
 # 导出镜像（普通/压缩）
