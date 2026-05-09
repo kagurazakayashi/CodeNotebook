4156a85a6d4b5c0d0088ca48c67a0a43761969d9
KagurazakaMiyabi (神楽坂雅詩) <miyabi@uuu.moe>
2026-05-09 20:56:20 +0800

    sync

--- JavaScript_Node/配置npm-g包的存放路径.bat
+++ JavaScript_Node/配置npm-g包的存放路径.bat
@@ -1,10 +1,16 @@
 TITLE 配置npm-g包的存放路径
 
-REM 设置全局安装路径
+REM 查看/设置全局安装路径
+npm config get prefix
 npm config set prefix "D:\npm"
 
-REM 设置缓存路径（可以放 RAMDisk 重启清空）
+REM 查看/设置缓存路径（可以放 RAMDisk 重启清空）
+npm config get cache
 npm config set cache "B:\TMP"
+
+REM 对应的 bash 环境变量
+@REM export NPM_CONFIG_PREFIX=`npm config get prefix`
+@REM export NPM_CONFIG_CACHE=`npm config get cache`
 
 REM 确认路径是否已更新
 npm config list
